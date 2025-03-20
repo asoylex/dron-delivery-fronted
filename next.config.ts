3f069836483bf06c50db8a12d53b413566074fb9
Antonio Cruz <asoylex@gmail.com>
2025-03-20 01:25:43 -0600

    Add client and admin home pages, login page, and update layout and configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  nextConfig: {
+    reactStrictMode: false,
+  },
 };
 
 export default nextConfig;
